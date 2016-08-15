aef3c77f916001518912b7e136ca47944e76b005
Dmitrij Vasiluk <vasilukwolf@gmail.com>
2016-08-15 18:12:34 +0300

    Add routes for submit laracast

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,7 @@
 */
 Route::get('about','PageController@about');
 Route::get('contact','PageController@contact');
+
+Route::get('cards','CardsController@store');
+Route::post('cards/create','CardsController@create');
+
